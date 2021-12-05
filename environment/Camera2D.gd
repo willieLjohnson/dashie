f06d930d0c980ea161c6063515f550fcd7e95f45
Willie Liwa Johnson <liwa.johnson@gmail.com>
2021-12-05 13:22:37 -0500

    Increase camera zoom based on player velocity

--- environment/Camera2D.gd
+++ environment/Camera2D.gd
@@ -2,8 +2,10 @@ extends Camera2D
 
 var screen_shake_started = false
 var shake_intensity = 0
-var zoom_factor = 0.0015
+var zoom_factor = 0.015
+var velocity_factor = 1
 var damp = 0.3
+
 
 onready var base_zoom = zoom
 
@@ -14,7 +16,8 @@ func _exit_tree() -> void:
 	Global.camera = null
 	
 func _process(delta: float) -> void:
-	zoom = lerp(zoom, base_zoom, damp)
+	velocity_factor = (1 + (Global.player.velocity.length() / 500))
+	zoom = lerp(zoom, (base_zoom * velocity_factor), damp)
 	
 	if screen_shake_started:
 		global_position += Vector2(rand_range(-shake_intensity, shake_intensity), rand_range(-shake_intensity, shake_intensity)) * delta
@@ -22,12 +25,13 @@ func _process(delta: float) -> void:
 		global_position = lerp(global_position, Vector2(320, 180), damp)
 		
 func screen_shake(intensity, time):
-	if intensity > shake_intensity:
-		zoom = base_zoom - Vector2(intensity * zoom_factor, intensity * zoom_factor)
-		shake_intensity = intensity
-		$ScreenShakeTime.wait_time = time
-		$ScreenShakeTime.start()
-		screen_shake_started = true
+	return
+#	if intensity > shake_intensity:
+#		zoom = (base_zoom * velocity_factor) - Vector2(intensity * zoom_factor, intensity * zoom_factor)
+#		shake_intensity = intensity
+#		$ScreenShakeTime.wait_time = time
+#		$ScreenShakeTime.start()
+#		screen_shake_started = true
 
 func _on_ScreenShakeTime_timeout() -> void:
 	screen_shake_started = false
